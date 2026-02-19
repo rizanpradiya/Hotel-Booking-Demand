9bc59575f1b7c947ab8e4d0d7ee3bfc7922894a3
rizanpradiya <137093891+rizanpradiya@users.noreply.github.com>
2026-02-19 22:17:53 +0700

    Refactor model loading and update file path

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import joblib
 # LOAD MODEL
 @st.cache_resource
 def load_model():
-    return joblib.load("main/hotel_cancellation_model.pkl")
+    return joblib.load("Hotel-Booking-Demand/hotel_cancellation_model.pkl")
 
 st.set_page_config(page_title="Hotel Cancellation Risk", page_icon="🏨")
 
@@ -167,4 +167,5 @@ if st.button("🔍 Predict Cancellation Risk"):
 
     except Exception as e:
         st.error(f"Prediction error: {e}")
+
 
